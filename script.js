ccf2901241579541a833c944f6b978bafe379463
Xia Yi <11078279+himangsu@user.noreply.gitee.com>
2022-06-06 22:39:01 +0800

    selected the score elements

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
